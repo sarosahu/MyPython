6439532621d7f06faa3c61b09f61889cef04bcb4
sarosahu <saroj.sahu@oracle.com>
2018-12-24 21:57:00 +0530

    Adding a program which demonstrates how we can unpack the sequence of data from a collection

---
+++ unpackingSequenceToVariable.py
@@ -0,0 +1,17 @@
+p = (4,5)
+x,y = p
+print(x,y)
+
+data = ['ACME', 50, 91.1, (2012, 12, 21)]
+
+name, shares, price, date = data
+
+print(name)
+print(shares)
+print(price)
+print(date)
+year, mon, day = date
+
+print(year)
+print(mon)
+print(day)
